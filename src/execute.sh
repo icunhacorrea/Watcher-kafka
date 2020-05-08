af028a3b9a2042d43512dee659449a931b82ce3c
icunhacorrea <iago.cunhac@gmail.com>
2020-05-08 16:01:26 -0300

    Watcher final para experimentação.

--- src/execute.sh
+++ src/execute.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-if [ $1 == "logger" ]; then
-	java -cp .:jars/kafka-clients-2.3.1.jar:jars/slf4j-api-1.7.28.jar:jars/logback-classic-1.2.3.jar:jars/logback-core-1.2.3.jar:jars/recordutil.jar \
-		main.WatcherKafka $2 $3 $4
-elif [ $1 == "nlogger" ]; then
-	java -cp .:jars/kafka-clients-2.3.1.jar:jars/slf4j-api-1.7.28.jar:jars/recordutil.jar \
-		main.WatcherKafka $2 $3 $4
-fi
+java -cp "WatcherKafka.jar:../jars/*" main.WatcherKafka
 
