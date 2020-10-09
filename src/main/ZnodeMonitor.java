786c5af752cf27df20e3df3fb328b825f4d78b50
icunhacorrea <iago.cunhac@gmail.com>
2020-10-09 16:55:26 -0300

    ...

--- src/main/ZnodeMonitor.java
+++ src/main/ZnodeMonitor.java
@@ -33,6 +33,7 @@ public class ZnodeMonitor extends Thread {
                             bytes = zk.getData(event.getPath(), false, null);
                             String data = new String(bytes);
                             System.out.println("Notificação: " + data);
+                            System.out.println("count: " + count);
 
                             circularList.addReceived(data);
 
