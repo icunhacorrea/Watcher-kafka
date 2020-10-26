bd748b90c7d17b5a48efbf476734278fc6cc20ee
icunhacorrea <iago.cunhac@gmail.com>
2020-10-26 13:37:53 -0300

    Ajustando idade

--- src/main/CircularList.java
+++ src/main/CircularList.java
@@ -141,7 +141,7 @@ public class CircularList {
                 // Se for velho demais, reenviar e alterar.
                 // resend current here.
 
-                if (current.getAge() > 0){
+                if (current.getAge() > 20){
                     replace = true;
                     break;
                 }
