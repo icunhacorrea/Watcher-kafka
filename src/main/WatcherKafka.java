6b613c6753604a47d53e39db052a5a105c6f2e18
icunhacorrea <iago.cunhac@gmail.com>
2020-10-26 14:27:02 -0300

    Ajustando idade

--- src/main/WatcherKafka.java
+++ src/main/WatcherKafka.java
@@ -6,7 +6,7 @@ public class WatcherKafka {
 
         //String zkHostPort = "localhost:2181";
         //String zkHostPort = "zoo1:2181,zoo2:2181,zoo3:2181";
-        String zkHostPort = "172.21.0.2:2181,172.21.0.3:2181,172.21.0.4:2181";
+        String zkHostPort = "172.21.0.2:2181,172.21.0.3:2181,172.21.0.4:2181,172.21.0.10:2181";
 
         String znode = "/brokers/topics";
 
